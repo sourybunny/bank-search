ed50a3939b8d5b8013366308d2aa101438483819
sourya <soury.bunny@gmail.com>
2019-01-29 18:26:10 +0530

    search branches in a city

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,51 +4,63 @@
      <v-flex xs4 >
         <v-select
           :items="cities"
-          v-model="select"
+          v-model="selectcity"
           outline
         ></v-select>
      </v-flex>
     <v-flex xs>
-        <v-autocomplete
+        <v-autocomplete :disabled="toggle()"
         append-icon="search"
         :loading="loading"
-        :items="cities"
+        :items="branches"
         :search-input.sync="search"
-        v-model="select"
-        cache-items
+        v-model="selectbranch"
+        
         class="mx-3"
         flat
         hide-no-data
         hide-details
-        label="What city are you from?"
-        @change="getCities"
+        label="What branch do you want?"
+        
         solo outline
         ></v-autocomplete>
       </v-flex>
     </v-layout>
     <v-layout>
       <v-flex>
-        <h1 v-if="banks.length >0">{{banks[3]}}</h1>
-        <h1 v-else-if="!search">enter a city</h1>
-        <h1  v-else>Loading banks in {{upperCase}} Please wait.</h1>
+        <h1 v-if="!selectcity">Please select a your city.</h1>
+        <h1 v-else-if="!search">Please wait! select/search for a branch </h1>
+        <div v-else>
+          <h1 v-if="!match">Loading details! Please wait.</h1>
+          <bank :banks="match"></bank>
+        </div>
       </v-flex>
     </v-layout>
+    
 </v-container>
 
 </template>
 <script>
+import Bank from './banks/Bank.vue';
   export default {
+    components:{
+      'bank': Bank
+    },
     data () {
       return {
         loading: false,
-        items: [],
         banks:[],
+        branches:[],
         search: '',
-        select: null,
+        selectcity: null,
+        selectbranch: null,
+        match: '',
         cities: [
             'Hyderabad',
+            'Visakhapatnam',
             'Mumbai',
             'Chennai',
+            'Patna',
             'Kolkata',
             'Bhubaneshwar',
             'Bangalore',
@@ -59,12 +71,17 @@
     },
     watch: {
       search (val) {
-        val && val !== this.select && this.querySelections(val)
         if(val){
-          this.getCities();
-          console.log(this.banks);
+          this.filterBranches(val);
         }
-
+        
+      },
+      selectcity(city) {
+        if(city){
+        this.getCities();
+        console.log(this.banks);
+        console.log(this.branches);
+        }
       }
     },
     methods: {
@@ -72,7 +89,7 @@
         this.loading = true
         // Simulated ajax query
         setTimeout(() => {
-          this.items = this.cities.filter(e => {
+          this.items = this.branches.filter(e => {
             return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
           })
           this.loading = false
@@ -80,24 +97,46 @@
       },
       getCities(){
         this.banks=[];
+        this.branches=[];
+        this.items=[];
         fetch(`https://vast-shore-74260.herokuapp.com/banks?city=${this.upperCase}`)
         .then(res=>res.json())
         .then(data=>{
           for(var bank in data){
             this.banks.push(data[bank])
+            this.branches.push(data[bank].branch);
           }
           
         })
         // console.log(this.banks);
+      },
+      toggle(){
+        if(this.banks.length<=0){
+          return true;
+        }
+          else{
+            return false;
+          }
+        
+      },
+      filterBranches(branchname){
+          this.match=[];
+          const result= this.banks.filter(bank=>{
+          return bank.branch==branchname
+        })
+        if(result){
+          console.log(result);
+          this.match= result;
+        }
       }
     },
     computed: {
       upperCase(){
-        if(this.search){
-          return this.search.toUpperCase()
+        if(this.selectcity){
+          return this.selectcity.toUpperCase()
         }
-        
       }
+    
     }
   }
 </script>
---
+++ src/components/banks/Bank.vue
@@ -0,0 +1,43 @@
+<template >
+<div v-if="banks">
+    
+<div>
+    <table style="width:100%">
+  <tr>
+    <th>Bank Name</th>
+    <th>IFSC</th> 
+    <th>Address</th>
+
+  </tr>
+  <tr v-for="bank in banks">
+    <td>{{bank.bank_name}}</td>
+    <td>{{bank.ifsc}}</td>
+    <td>{{bank.address}}</td>
+  </tr>
+ 
+</table>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    props:['banks']
+}
+</script>
+
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+
+</style>
